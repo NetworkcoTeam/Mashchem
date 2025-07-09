866fe324a1e61209306663596c42a7e2bf377184
flash3105 <flash3105findme@gmail.com>
2025-07-09 14:54:27 +0200

    updates2

--- Navbar.js
+++ Navbar.js
@@ -1,11 +1,11 @@
-// Navbar.js - Navigation Component
+// Navbar.js - Navigation Component with Icons
 export function renderNavbar(containerId = "app") {
     const navbarHTML = `
     <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
       <div class="container">
-        <a class="navbar-brand" href="#" id="logo">
-          <img src="images/Maschem-logo.png" alt="Maschem Logo" class="maschem-logo"/>
-          Maschem
+        <a class="navbar-brand d-flex align-items-center" href="#" id="logo">
+          <img src="images/Maschem-logo.png" alt="Maschem Logo" class="maschem-logo me-2"/>
+          <span class="fw-bold">Maschem</span>
         </a>
         
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -15,16 +15,28 @@ export function renderNavbar(containerId = "app") {
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-              <a class="nav-link active" id="home-link" href="#">Home</a>
+              <a class="nav-link active d-flex align-items-center" id="home-link" href="#">
+                <i class="bi bi-house-door me-2"></i>
+                <span>Home</span>
+              </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#" id="products-link">Products</a>
+              <a class="nav-link d-flex align-items-center" href="#" id="products-link">
+                <i class="bi bi-box-seam me-2"></i>
+                <span>Products</span>
+              </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#" id="about-link">About Us</a>
+              <a class="nav-link d-flex align-items-center" href="#" id="about-link">
+                <i class="bi bi-building me-2"></i>
+                <span>About Us</span>
+              </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#" id="contact-link">Contact</a>
+              <a class="nav-link d-flex align-items-center" href="#" id="contact-link">
+                <i class="bi bi-telephone me-2"></i>
+                <span>Contact</span>
+              </a>
             </li>
           </ul>
         </div>
